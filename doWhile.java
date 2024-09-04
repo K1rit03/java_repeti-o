a2b8cd7df04705aa7ed044016fd9c2ccdf59d9e0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 16:06:28 -0300

    Do while exemplo

---
+++ doWhile.java
@@ -0,0 +1,10 @@
+public class doWhile {
+    public static void main(String[] arg) {
+        int i = 1;
+        do {
+            System.out.println("I = " + i);
+            i++;
+        } while (i <= 3);
+    }
+
+}
